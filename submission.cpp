ab0712f2f56ed129fa671692b730d4a047b09f5c
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-15 00:49:01 -0400

    Update submission.cpp

--- submission.cpp
+++ submission.cpp
@@ -11,57 +11,99 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   
-  int maxSize;     
+	int maxSize;     
+	vector<int> heap;
+	vector<string> trace;
 	fstream file(argv[1], fstream::in);
 	ofstream output;
 	output.open("argv[2]");
   
 	if(file.is_open()){
-		file >> maxSize;
-		while(!file.eof() && !file.fail()) {
-		  string operation;
-      file >> operation;
-      if(operation=="findContestant") {
-        string id;
-        file >> id;
+	file >> maxSize;
+	int [maxSize+1] 
+while(!file.eof() && !file.fail()) {
+      	string operation;
+      	file >> operation;
+	
+      if(operation=="findContestant") { 
+      	 string id;
+      	 file >> id;
+	 boolean exists = false;
+	 output << "findContestant " << id << " " << score;
+	 for(int i = 0; i<trace.size(); i++) {
+		 if(trace[i]==id) {
+			exists = true;
+			output << "Contestant " << id << " is in the extended heap with score " << heap[i] << ".";	 
+		 }
+	 }
+	 if(exists==false) {
+		output << "Contestant " << id << " is not in the extended heap.";
+	 }
       }
       else if(operation=="insertContestant") {
         string id, score;
         file >> id;
         file >> score;
-        stringstream num(score[1]); 
+	output << "insertContestant " << id << " " << score;
+        stringstream num1(score[1]);
+	stringstream num2(score[1]);
         int x = 0;
-        num >> x; 
-        output << "insertContestant " <<
-        output << "Contestant " << id << " inserted with initial score " << score << "."
+	int y = 0;
+        num1 >> x; 
+	num2 >> y;
+       
+        output << "Contestant " << id << " inserted with initial score " << score << ".";
       }
       else if(operation=="eliminateWeakest") {
-        
+	output << "eliminateWeakest";
+	if(heap.size()==0) {
+        	output << "No contestant can be eliminated since the extended heap is empty." <<;
+	}
+	else {
+		string id = trace[trace.size()-1];
+		int score = heap[heap.size()-1];
+		output << "Contestant " << id << " with current lowest score " << score << "eliminated.";
+		trace.erase(trace.size()-1);
+		heap.erase(heap.size()-1);
+	}
       }
       else if(operation=="earnPoints") {
         string id, points;
         file >> id;
         file >> points;
+	output << "earnPoints " << id << " " << points;
+	stringstream num(points[1]); 
+        int x = 0;
+        num >> x; 
       }
       else if(operation=="showContestants") {
-      
+      	output << "showContestants";
+	 for(int i = 0; i<trace.size(); i++) {
+		output << "Contestant " << id << " in extended heap location <"<< i+1 << "> with score <" << heap[i] << ">.";
+	 }	      
       }
       else if(operation=="losePoints") {
         string id, points;
         file >> id;
         file >> points;
+	output << "losePoints " << id << " " << points;
+	stringstream num(points[1]); 
+        int x = 0;
+        num >> x; 
       }
       else if(operation=="showHandles") {
-      
+      	output << "showHandles";
       }
       else if(operation=="showLocation") {
         string id;
         file >> id;
+	output << "showLocation " << id
       }
       else if(operation=="crownWinner") {
-      
+      	output << "crownWinner";
+	string id, points;
       }
-		}
+	}
 	file.close();
 	}     
 }
